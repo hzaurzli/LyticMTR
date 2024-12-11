73e5378f8545de586416990f1a107e4bdced64dc
Small runze <yun_act@163.com>
2024-12-11 16:31:35 +0800

    Add files via upload

--- MLPC/src/random_sample.py
+++ MLPC/src/random_sample.py
@@ -2,6 +2,17 @@ import numpy as np
 import argparse
 
 def main(dict1,dict2,k):
+  dict3 = dict1
+  random_keys = np.random.choice(list(dict1.keys()), k, replace = False)
+  for random_key in random_keys:
+    random_value = dict1[random_key]
+    dict2[random_key] = random_value
+    del dict3[random_key]
+    
+  return dict2,dict3
+
+
+def main_test(dict1,dict2,k):
   random_keys = np.random.choice(list(dict1.keys()), k, replace = False)
   for random_key in random_keys:
     random_value = dict1[random_key]
@@ -11,10 +22,12 @@ def main(dict1,dict2,k):
 
 
 if __name__ == '__main__':
-  parser = argparse.ArgumentParser(description="Random sample")
+  parser = argparse.ArgumentParser(description="random sample")
   parser.add_argument("-i", "--input_file", required=True, type=str, help="input feature table (txt,'\t')")
-  parser.add_argument("-o", "--output_file", required=True, type=str, help="output feature table (after sample, txt,'\t')")
-  parser.add_argument("-k", "--sample_k", required=True, type=int, help="sample number")
+  parser.add_argument("-or", "--output_train_file", required=True, type=str, help="output training feature table (after sample, txt,'\t')")
+  parser.add_argument("-oe", "--output_test_file", required=True, type=str, help="output testing feature table (after sample, txt,'\t')")
+  parser.add_argument("-kr", "--sample_kr", required=True, type=int, help="sample number for training")
+  parser.add_argument("-ke", "--sample_ke", required=True, type=int, help="sample number for testing")
   Args = parser.parse_args()
   
   f = open(Args.input_file)
@@ -26,10 +39,20 @@ if __name__ == '__main__':
     val = '\t'.join(i.strip().split('\t')[1:])
     old_dict[key] = val
   
-  new_dict = main(old_dict,new_dict,Args.sample_k)
+  new_dict,old_test_dict = main(old_dict,new_dict,Args.sample_kr)
   
-  with open(Args.output_file,'w') as w:
+  with open(Args.output_train_file,'w') as w:
     for key in new_dict:
       line = key + '\t' + new_dict[key] + '\n'
+      w.write(line)
+  w.close()
+  
+  new_test_dict = {}
+  #print(test_key)
+  new_test_dict = main_test(old_test_dict,new_test_dict,Args.sample_ke)
+  
+  with open(Args.output_test_file,'w') as w:
+    for key in new_test_dict:
+      line = key + '\t' + new_test_dict[key] + '\n'
       w.write(line)
   w.close()
