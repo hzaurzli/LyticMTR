13ff882c2dec341936b420aeac8c140d5d370a75
Small runze <yun_act@163.com>
2024-12-12 16:57:05 +0800

    Update test.py

--- MLPC/DL_model/test.py
+++ MLPC/DL_model/test.py
@@ -12,6 +12,7 @@ from keras.models import load_model
 from keras.backend import expand_dims
 import numpy as np
 import pandas as pd
+import argparse
 
 
 def catch(data, label):
@@ -43,11 +44,11 @@ def catch(data, label):
     return data, label
 
 
-def predict(X_test, y_test, para, weights, jsonFiles, h5_model, dir):
+def predict(X_test, y_test, para, h5_model):
     
     adam = Adam(lr=para['learning_rate']) # adam optimizer
     # 1.loading weight and structure (model)
-    h5_model_path = '/home/user/Desktop/yyf_data/MLBP/MLBP/model.h5'
+    h5_model_path = 
     load_my_model = load_model(h5_model_path, custom_objects={'keras.backend': keras.backend, 'expand_dims': expand_dims}) # 需要传一个custom_objects参数,将自定义的层添加进去
     print("Prediction is in progress")
 
@@ -74,7 +75,7 @@ def predict(X_test, y_test, para, weights, jsonFiles, h5_model, dir):
           laber.append('0')
       functions.append(laber)
 
-    output_file = os.path.join('/home/user/Desktop/yyf_data/MLBP/MLBP/', 'result.txt')
+    output_file = './result.txt'
     with open(output_file, 'w') as f:
         for i in functions:
             f.write('\t'.join(i) + '\n')
@@ -96,62 +97,63 @@ def predict(X_test, y_test, para, weights, jsonFiles, h5_model, dir):
     print('\n')
 
 
-def test_my(test, para, dir):
+def test_my(test, para):
     # step1: preprocessing
     test[1] = keras.utils.to_categorical(test[1])
     test[0], temp = catch(test[0], test[1])
     temp[temp > 1] = 1
     test[1] = temp
-
-    # weight and json
-    weights = []
-    jsonFiles = []
-    h5_model = []
-    weights.append('model.hdf5')
-    jsonFiles.append('model.json')
-    h5_model.append('model.h5')
         
     # step2:predict
     predict(test[0], test[1], para, weights, jsonFiles, h5_model, dir)
     
 if __name__ == '__main__':
-  f = open('/home/user/Desktop/yyf_data/laber/random_sample/final_input.txt')
-  g = open('/home/user/Desktop/yyf_data/laber/random_sample/laber.txt')
-  
-  max_seq_len = 500
-  feats = []
-  for i in f:
-    feat_property = i.strip().split('\t')[1:6]
-    feat_other = i.strip().split('\t')[6:]
-    feat_property_padding = [0] * int(max_seq_len - len(feat_property))
-    feat_property.extend(feat_property_padding)
-    feat_property.extend(feat_other)
-    feat_tmp = list(map(float,feat_property))
-    feats.append(feat_tmp)
+    parser = argparse.ArgumentParser(description="Training")
+    parser.add_argument("-iX", "--input_X", required=True, type=str, help="feature table (txt,'\t')")
+    parser.add_argument("-iy", "--input_y", required=True, type=str, help="laber file (txt, '\t')")
+    parser.add_argument("-m", "--model_path", required=True, type=str, help="model path (.h5)")
+    Args = parser.parse_args()  
+      
+    f = open(Args.input_X)
+    g = open(Args.input_y)
+    h5_model = Args.model_path
     
-  laber_tmp = []
-  for j in g:
-    laber = j.strip().split('\t')[0]
-    num = int(j.strip().split('\t')[1])
-    laber_tmp.extend(num*[laber])
+    # f = open('/home/user/Desktop/yyf_data/laber/random_sample/final_input.txt')
+    # g = open('/home/user/Desktop/yyf_data/laber/random_sample/laber.txt')
     
-  labers = list(map(int,laber_tmp))
+    max_seq_len = 500
+    feats = []
+    for i in f:
+      feat_property = i.strip().split('\t')[1:6]
+      feat_other = i.strip().split('\t')[6:]
+      feat_property_padding = [0] * int(max_seq_len - len(feat_property))
+      feat_property.extend(feat_property_padding)
+      feat_property.extend(feat_other)
+      feat_tmp = list(map(float,feat_property))
+      feats.append(feat_tmp)
+      
+    laber_tmp = []
+    for j in g:
+      laber = j.strip().split('\t')[0]
+      num = int(j.strip().split('\t')[1])
+      laber_tmp.extend(num*[laber])
+      
+    labers = list(map(int,laber_tmp))
+    
+    feats_dat = np.array(feats)
+    labers_dat = np.array(labers)
+    
+    feats_train, feats_test, labels_train, labels_test = train_test_split(feats_dat, labers_dat, test_size=0.1, random_state=0)
+           
+    test = [feats_test, labels_test]
   
-  feats_dat = np.array(feats)
-  labers_dat = np.array(labers)
-  
-  feats_train, feats_test, labels_train, labels_test = train_test_split(feats_dat, labers_dat, test_size=0.1, random_state=0)
-         
-  test = [feats_test, labels_test]
-
-  # parameters
-  ed = 100
-  ps = 5
-  fd = 128
-  dp = 0.5
-  lr = 0.001
-  para = {'embedding_dimension': ed, 'pool_size': ps, 'fully_dimension': fd,
-          'drop_out': dp, 'learning_rate': lr}
-  
-  dir = '/home/user/Desktop/yyf_data/MLBP/MLBP/'
-  test_my(test, para, dir)
+    # parameters
+    ed = 100
+    ps = 5
+    fd = 128
+    dp = 0.5
+    lr = 0.001
+    para = {'embedding_dimension': ed, 'pool_size': ps, 'fully_dimension': fd,
+            'drop_out': dp, 'learning_rate': lr}
+    
+    test_my(test, para)
